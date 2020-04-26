d5c0da91fe66a900a284f45aebea0715198692e3
Michal Karwowski <michal.karwowski.0@gmail.com>
2020-04-26 21:09:21 +0200

    cors configuration

--- src/main/java/pl/michalkarwowski/processmodeler/configurations/Beans.java
+++ src/main/java/pl/michalkarwowski/processmodeler/configurations/Beans.java
@@ -3,6 +3,9 @@ package pl.michalkarwowski.processmodeler.configurations;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class Beans {
@@ -10,5 +13,21 @@ public class Beans {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
